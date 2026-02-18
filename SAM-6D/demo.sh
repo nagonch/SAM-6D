0acb8cc4497453657fd16fa162f3c100b4611e4b
Your Name <you@example.com>
2026-02-18 20:29:01 +1100

    convert models

--- SAM-6D/demo.sh
+++ SAM-6D/demo.sh
@@ -1,4 +1,10 @@
 # Render CAD templates
+export CAD_PATH=/home/ngoncharov/cvpr2026/SAM-6D/SAM-6D/Data/Example/obj_000005.ply    # path to a given cad model(mm)
+export RGB_PATH=/home/ngoncharov/cvpr2026/SAM-6D/SAM-6D/Data/Example/rgb.png           # path to a given RGB image
+export DEPTH_PATH=/home/ngoncharov/cvpr2026/SAM-6D/SAM-6D/Data/Example/depth.png       # path to a given depth map(mm)
+export CAMERA_PATH=/home/ngoncharov/cvpr2026/SAM-6D/SAM-6D/Data/Example/camera.json    # path to given camera intrinsics
+export OUTPUT_DIR=/home/ngoncharov/cvpr2026/SAM-6D/SAM-6D/Data/Example/outputs 
+
 cd Render
 blenderproc run render_custom_templates.py --output_dir $OUTPUT_DIR --cad_path $CAD_PATH #--colorize True 
 
